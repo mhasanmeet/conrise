5f6e971ec770ce71578458a6ccc53a31a564fdf1
mhasan <mhasan.meet@gmail.com>
2025-03-11 04:00:46 +0600

    home: cart-section

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
 // main js file
 
+// color Swatches Btn
+document.querySelectorAll(".colorSwatchesBtn").forEach(button => {
+    button.addEventListener("click", function() {
+        this.classList.toggle("btn-light");
+    });
+});
+
 // Footer
 document.getElementById("copyrightYear").textContent = new Date().getFullYear();
